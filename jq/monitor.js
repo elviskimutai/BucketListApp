757a53e40cc1c92f7515dfdedaf90cf1ecfaa810
Genius <elviskcheruiyot@gmail.com>
2017-07-08 14:32:41 +0300

    added all filles commit

---
+++ jq/monitor.js
@@ -0,0 +1,12 @@
+$('#hideshow').toggle(function(){
+
+$('#hideshow').text('show');
+
+},function(){
+$('#hideshow'.text('hide');
+
+});
+
+$('#hideshow').click(function(){
+$('#message').toggle();
+});
